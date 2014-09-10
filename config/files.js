d68296fe75ece317a375cf47fe4336b61f39922e
Hector Sansores <hector.sansores@virtus.com.mx>
2014-09-10 13:33:42 -0500

    Replace underscore with lodash.

--- config/files.js
+++ config/files.js
@@ -9,15 +9,11 @@ module.exports = function(lineman) {
   //Override file patterns here
   return {
 
-    // As an example, to override the file patterns for
-    // the order in which to load third party JS libs:
-    //
-    // js: {
-    //   vendor: [
-    //     "vendor/js/underscore.js",
-    //     "vendor/js/**/*.js"
-    //   ]
-    // }
+   js: {
+     vendor: [
+       "vendor/js/lodash.js"
+     ]
+   }
 
   };
 };
