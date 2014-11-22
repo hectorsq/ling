4b0e84c33d053baeef70b562bc208168894c2725
Hector Sansores <hector.sansores@virtus.com.mx>
2014-11-22 13:14:37 -0600

    Update angular to 1.3.3

--- config/files.js
+++ config/files.js
@@ -11,7 +11,7 @@ module.exports = function(lineman) {
 
     js: {
       vendor: [
-        "vendor/js/angular.js",
+        "vendor/js/angular-1.3.3.js",
         "vendor/js/lodash.js",
         "vendor/js/angular-ui-router.js",
         "vendor/js/ui-bootstrap-tpls-0.11.0.js",
