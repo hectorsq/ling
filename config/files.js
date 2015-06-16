101c6921bdcb3245ad30c75a65ff83d24dd2ccc6
Hector Sansores <hector.sansores@virtus.com.mx>
2015-06-16 18:09:41 -0500

    Upgrade lodash to 3.9.3

--- config/files.js
+++ config/files.js
@@ -12,7 +12,7 @@ module.exports = function(lineman) {
     js: {
       vendor: [
         "vendor/js/angular-1.3.16.js",
-        "vendor/js/lodash.js",
+        "vendor/js/lodash-3.9.3.js",
         "vendor/js/angular-ui-router-0.2.15.js",
         "vendor/js/ui-bootstrap-tpls-0.13.0.js",
         "vendor/js/angular-devise-1.1.0.js"
