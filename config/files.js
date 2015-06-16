80f3e56a14e9b554e58b4cf2f61d010e2dc31a31
Hector Sansores <hector.sansores@virtus.com.mx>
2015-06-16 18:04:42 -0500

    Upgrade angular ui bootstrap to 0.13.0

--- config/files.js
+++ config/files.js
@@ -14,7 +14,7 @@ module.exports = function(lineman) {
         "vendor/js/angular-1.3.16.js",
         "vendor/js/lodash.js",
         "vendor/js/angular-ui-router-0.2.15.js",
-        "vendor/js/ui-bootstrap-tpls-0.12.0.js",
+        "vendor/js/ui-bootstrap-tpls-0.13.0.js",
         "vendor/js/angular-devise-1.1.0.js"
       ]
     }
