66bb66532a2db48e56f3674c241030225f54033d
Hector Sansores <hector.sansores@virtus.com.mx>
2015-06-16 17:25:37 -0500

    Upgrade ui-router to 0.2.15

--- config/files.js
+++ config/files.js
@@ -13,7 +13,7 @@ module.exports = function(lineman) {
       vendor: [
         "vendor/js/angular-1.3.16.js",
         "vendor/js/lodash.js",
-        "vendor/js/angular-ui-router-0.2.13.js",
+        "vendor/js/angular-ui-router-0.2.15.js",
         "vendor/js/ui-bootstrap-tpls-0.12.0.js",
         "vendor/js/devise-1.0.0.js"
       ]
